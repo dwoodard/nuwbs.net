b814ddd0ca35ad0a5739b413cad3974126c3d04a
Dustin Woodard <dustin.woodard@gmail.com>
2014-06-27 10:03:50 -0600

    xmind

--- app/Nuwbs/Users/User.php
+++ app/Nuwbs/Users/User.php
@@ -1,9 +1,4 @@
-<?php
-
-use Illuminate\Auth\UserTrait;
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
+<?php namespace Nuwbs\Users;
 
 class User extends BaseModel implements UserInterface, RemindableInterface {
 
