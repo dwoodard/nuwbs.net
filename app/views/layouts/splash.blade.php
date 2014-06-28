0324cede3cd5f7664f0a092c55cb1fc23f0e9e8d
Dustin Woodard <dustinwoodard@weber.edu>
2014-06-27 18:39:02 -0600

    launch page

--- app/views/layouts/splash.blade.php
+++ app/views/layouts/splash.blade.php
@@ -3,11 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css"/>
+    <link rel="stylesheet" href="/bower/normalize-css/normalize.css"/>
+    <link href='http://fonts.googleapis.com/css?family=Scada:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/bower/bootstrap/dist/css/bootstrap.min.css"/>
 </head>
 <body>
 
 
-Splash Page
+<header id="main-nav">
+    <div class="container">
+        <img src="/img/nuwbs-logo.png" alt=""/>
+    </div>
+</header>
 
+<div id="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-3 col-xs-offset-3">
+            <div id="counter">
+                Counter
+            </div>
+            <!--    <div id="promo">Video</div>-->
+            <div id="mailchimp">
+                <!-- Begin MailChimp Signup Form -->
+                <link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+                <style type="text/css">
+                    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                </style>
+                <div id="mc_embed_signup">
+                    <form action="http://nuwbs.us8.list-manage.com/subscribe/post?u=ee95f6cfa88ac4bfcd0f6d4bd&amp;id=c6763e8b8d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                        <label for="mce-EMAIL">Interested? We'll let you know when it's ready</label>
+                        <input type="email" value="" name="EMAIL" class="email input-lg ui-spinner-input" id="mce-EMAIL" placeholder="Email" required>
+                        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                        <div style="position: absolute; left: -5000px;"><input type="text" name="b_ee95f6cfa88ac4bfcd0f6d4bd_c6763e8b8d" tabindex="-1" value=""></div>
+                        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                    </form>
+                </div>
+
+                <!--End mc_embed_signup-->
+            </div>
+
+        </div>
+        </div>
+    </div>
+</div>
+
+<footer>
+
+</footer>
+
+
+<script src="/bower/jquery/dist/jquery.js"></script>
+<script src="/bower/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<script>
+    $(document).ready(function(){
+        console.log('working')
+    })
+</script>
 </body>
 </html>
